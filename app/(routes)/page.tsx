ce03b98475c7ce21922f2afffb457d512d844542
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-03-01 00:00:51 +0100

    feat: update app page and layout

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@/components/shared/container";
+
+const HomePage = () => {
+  return (
+    <main>
+      <Container>
+        <div>
+          
+        </div>
+      </Container>
+    </main>
+  );
+};
+
+export default HomePage;
