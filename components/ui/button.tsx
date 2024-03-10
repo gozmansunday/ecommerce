2a3a344812d54641938656181aee77921ec004cd
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-03-10 10:07:51 +0100

    feat: add and update ui components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -25,6 +25,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-14 rounded-md px-12 md:h-16",
         custom: "py-1.5 px-4 rounded-full",
         icon: "h-9 w-9",
         lgIcon: "h-10 w-10",
