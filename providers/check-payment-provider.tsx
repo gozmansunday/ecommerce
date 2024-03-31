caaadec38e6a374c4f3535b4ac66849c36c3a0eb
Chiagoziem Sunday <gozmansunday@gmail.com>
2024-04-01 00:59:20 +0100

    feat: create providers for tanstack query and checking stripe payments

---
+++ providers/check-payment-provider.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+// Global Imports
+import { useSearchParams } from "next/navigation";
+import { ReactNode, useEffect } from "react";
+import { toast } from "sonner";
+
+// Local Imports
+import { useBag } from "@/hooks/useBag";
+
+interface Props {
+  children: ReactNode;
+};
+
+export const CheckPaymentProvider = ({ children }: Props) => {
+  const searchParams = useSearchParams();
+  const { removeAll } = useBag();
+
+  useEffect(() => {
+    if (searchParams.get("success")) {
+      toast.success("Payment complete.");
+      removeAll();
+    }
+
+    if (searchParams.get("canceled")) {
+      toast.error("Purchase canceled!");
+    }
+  }, [searchParams, removeAll]);
+
+  return (
+    <>
+      {children}
+    </>
+  );
+};
---
+++ providers/query-provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+// Global Imports
+import { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+interface Props {
+  children: ReactNode;
+};
+
+const QueryProvider = ({ children }: Props) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
+  );
+};
+
+export default QueryProvider;
